f114059935b3dc8974566a0d8b1f5f2c3dc75b5d
Derek Moore <moore8577@gmail.com>
2018-11-05 21:05:14 -0800

    Basic layout of the concept

---
+++ hangman.js
@@ -0,0 +1,14 @@
+
+// Create an array of words
+// Choose words randomly
+// Create underscores based on wordlength
+// Get User Guess
+// Check if it is right
+// if right, push to 'right' array
+// if wrong, push to 'wrong' array, and remove 1 life
+
+
+
+
+// PIE IN THE SKY - It would be cool to have this set to a classic arcade game, like pitfall or maybe a movie like Indiana Jones and the Temple of Doom.
+
